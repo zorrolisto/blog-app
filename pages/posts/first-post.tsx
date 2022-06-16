9658c2ec0ce56369737e8561d2afd34c7a975931
julio@theowl.solutions <julio@theowl.solutions>
2022-06-16 00:02:43 -0500

    Update: first-post

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,9 +1,13 @@
 import { FunctionComponent } from "react";
 import Link from "next/link";
+import Head from "next/head";
 
 const Header: FunctionComponent = () => {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>;
       <h2>
         <Link href="/">
